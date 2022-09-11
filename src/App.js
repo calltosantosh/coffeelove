18e9ba3921a6f7631a9bd2a4377ab9f6fe4df5ed
calltosantosh <callstosantosh@gmail.com>
2022-09-11 11:58:09 +0530

    Update App.js
    testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <iframe src="https://giphy.com/embed/TLn3LnsLMxhfyy7LuU" width="480" height="322" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/coffee-espresso-pixelprophecy-TLn3LnsLMxhfyy7LuU"></a></p> 
-        <h2>I love Tea</h2>
+        <h2>I am happy with my student effort</h2>
       </header>
     </div>
   );
